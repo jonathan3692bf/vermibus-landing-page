9e2057246db2a733c6ddd958c3d1cf682f0e9cb8
Jonathan <jonathan3692bf@users.noreply.github.com>
2021-04-25 13:34:31 +0200

    Add art cards

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ const exhibits = [
         }
       },
       {
-        coordinates: { x: 52.46146778, y: 13.32513895 },
+        coordinates: { x: 52.46140147, y: 13.32517213 },
         link: 'https://www.youtube.com/watch?v=FgUcRVH8NHU',
         previewImage: {
           src: '/images/cards/2.png',
@@ -31,7 +31,7 @@ const exhibits = [
         }
       },
       {
-        coordinates: { x: 52.46146778, y: 13.32513895 },
+        coordinates: { x: 52.46146778, y: 13.32513896 },
         link: 'https://www.youtube.com/watch?v=hKy2Gm2lRtM',
         previewImage: {
           src: '/images/cards/3.png',
@@ -40,7 +40,7 @@ const exhibits = [
         }
       },
       {
-        coordinates: { x: 52.46146778, y: 13.32513895 },
+        coordinates: { x: 52.46144351, y: 13.32514750 },
         link: 'https://www.youtube.com/watch?v=pjBkpsBFr_E',
         previewImage: {
           src: '/images/cards/4.png',
@@ -96,13 +96,12 @@ export default function HomePage () {
           In collaboration with Experience Designer <a href="https://juancacardell.com" className="text-[#fdefa6] font-normal">Juanca Cardell</a>, this project has been conceptualized and developed to challenge the ephemerality of conventional urban interventions, allowing the viewer to experience Vermibus’ installations long after they are gone. This project represents an innovative attempt to solve one of the biggest problems when exhibiting street art - the lack of its original context.
         </div>
       </div>
-      {exhibits.map(exhibit => {
-        const { previewImage } = exhibit
-        return <div className="py-4">
+      {exhibits.map(({ dates, location, vrLink, previewImage, artwork }) => {
+        return <div className="py-4" key={location}>
           <div className="relative" style={{ background: 'linear-gradient(black, transparent 20% 80%, black)' }}>
             <Image
               className="absolute z-[-1]"
-              alt={`Artwork in ${exhibit.location}`}
+              alt={`Artwork in ${location}`}
               src={previewImage.src}
               quality={80}
               // layout="fill"
@@ -114,10 +113,10 @@ export default function HomePage () {
           </div>
           <div className="flex items-center justify-between mt-4 sm:justify-start sm:space-x-4">
             <div className="flex flex-col">
-              <div className="text-xl font-bold uppercase">{exhibit.location}</div>
-              <div className="text-sm font-medium">{exhibit.dates[0]} - {exhibit.dates[1]}</div>
+              <div className="text-xl font-bold uppercase">{location}</div>
+              <div className="text-sm font-medium">{dates[0]} - {dates[1]}</div>
             </div>
-            <a href={exhibit.vrLink} target="_blank" rel="noreferrer">
+            <a href={vrLink} target="_blank" rel="noreferrer">
               <div className="p-[6px] border-2 border-white rounded-full w-10 h-10">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
@@ -128,20 +127,38 @@ export default function HomePage () {
           <div className="py-2 text-sm font-light leading-tight">
             In the first collection, the artist presents a series of four interventions installed in Berlin’s futuristic brutalist subway station U-Bhf. Schloßstraße, which were displayed for five days before being replaced by the OoH advertising company.
           </div>
+          <div className="grid gap-8 md:grid-cols-2">
+            {artwork.map(({ coordinates, link, previewImage }, index) => {
+              return <div key={`${coordinates.x}-${coordinates.y}`} className="relative">
+                <Image
+                  className="absolute z-[-1] rounded"
+                  alt={`Artwork in ${location}, piece number ${index + 1}`}
+                  src={previewImage.src}
+                  quality={100}
+                  layout="responsive"
+                  width={previewImage.width}
+                  height={previewImage.height}
+                />
+                <div className="absolute bottom-0 flex items-center justify-between w-full px-2 sm:px-4">
+                  <div className="flex flex-col">
+                    <div className="text-sm font-bold uppercase">{location} {index + 1} / {artwork.length}</div>
+                    <div className="space-x-2 text-xs font-thin">
+                      <span>X: {coordinates.x}</span>
+                      <span>Y: {coordinates.y}</span>
+                    </div>
+                  </div>
+                  <a href={link} target="_blank" rel="noreferrer">
+                    <div className="px-3 pt-2 pb-1 text-xs font-bold text-black uppercase bg-white rounded-full md:text-sm">
+                      View 360/VR
+                    </div>
+                  </a>
+                </div>
+              </div>
+            })}
+          </div>
         </div>
       })}
-
     </div>
-    {/* <div className="absolute bottom-0 flex w-full pt-18">
-        <div className="max-w-screen-lg px-4 mx-auto">
-          <h1 className="text-2xl font-bold">
-            VERMIBUS “IMMERSION”
-          </h1>
-          <h2 className="text-lg leading-tight">
-            An ongoing series of documented urban interventions in which the artist emphasizes the context of his work through fully immersive 360 VR experiences.
-          </h2>
-        </div>
-      </div> */}
   </main>
 </div>
 }
