53abead08bbe82a1254a7cdf7284305297d40779
Jonathan <jonathan3692bf@users.noreply.github.com>
2021-04-28 19:53:48 +0200

    Crop og:image

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function HomePage () {
 
     <meta property="og:url" content="https://vermibus-immersion.com"/>
     <meta property="og:type" content="website"/>
-    <meta property="og:image" content="https://vermibus-immersion.com/images/og-image.jpg" key="image" />
+    <meta property="og:image" content="https://vermibus-immersion.com/og_image.jpg" key="image" />
     <meta property="og:title" content="VERMIBUS “IMMERSION”"/>
     <meta property="og:description" content="An ongoing series of documented urban interventions in which the artist emphasizes the context of his work through fully immersive 360 VR experiences."/>
 
@@ -25,7 +25,7 @@ export default function HomePage () {
     <meta property="twitter:url" content="https://vermibus-immersion.com"/>
     <meta name="twitter:title" content="VERMIBUS “IMMERSION”"/>
     <meta name="twitter:description" content="An ongoing series of documented urban interventions in which the artist emphasizes the context of his work through fully immersive 360 VR experiences."/>
-    <meta name="twitter:image" content="https://vermibus-immersion.com/images/og-image.jpg"/>
+    <meta name="twitter:image" content="https://vermibus-immersion.com/og_image.jpg"/>
 
     <link rel="icon" href="/favicon.png" />
   </Head>
